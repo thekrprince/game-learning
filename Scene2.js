ca0ea8a2c14b72d92f68e6d7248822b0aa5ba66b
princekr96 <princekr700@gmail.com>
2023-04-03 20:31:54 +0530

    initial commit - ship falling

---
+++ Scene2.js
@@ -0,0 +1,35 @@
+class Scene2 extends Phaser.Scene {
+  constructor() {
+    super("playGame");
+  }
+
+  create() {
+    this.background = this.add.image(0, 0, "background");
+    this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
+    this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
+    this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+
+    this.add.text(20, 20, "Playing game", {font: "25px", fill: "yellow"});
+  }
+
+  moveShip(ship, speed) {
+    ship.y += speed;
+    if(ship.y > config.height) {
+      this.resetShipPos(ship);
+    }
+  }
+
+  resetShipPos(ship) {
+    ship.y = 0;
+    var randomX = Phaser.Math.Between(0, config.width);
+    ship.x = randomX;
+  }
+
+  update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
+  }
+}
---
+++ game.js
@@ -0,0 +1,8 @@
+var config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2]
+}
+
+var game = new Phaser.Game(config);
