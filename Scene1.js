0c269c1dd901766a2cb3a127a9fb41c85ce82bea
princekr96 <princekr700@gmail.com>
2023-04-04 21:24:48 +0530

    made ships clickable to blast them

--- Scene1.js
+++ Scene1.js
@@ -5,9 +5,29 @@ class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image("background", "assets/background.png");
-    this.load.image("ship", "assets/ship.png");
-    this.load.image("ship2", "assets/ship2.png");
-    this.load.image("ship3", "assets/ship3.png");
+    // this.load.image("ship", "assets/ship.png");
+    // this.load.image("ship2", "assets/ship2.png");
+    // this.load.image("ship3", "assets/ship3.png");
+
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
   }
 
   create() {
--- Scene2.js
+++ Scene2.js
@@ -7,11 +7,57 @@ class Scene2 extends Phaser.Scene {
     this.background = this.add.image(0, 0, "background");
     this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
-    this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
-    this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+    // updated from image to sprite
+    this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
+    this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
+    this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
+
+    // create animations
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3"),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion"),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+
+    // play the animations
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    // making ships clickable to destroy them
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    this.input.on('gameobjectdown', this.destroyShip, this);
 
     this.add.text(20, 20, "Playing game", {font: "25px", fill: "yellow"});
+  }
+
+  update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip(this.ship3, 3);
   }
 
   moveShip(ship, speed) {
@@ -27,9 +73,8 @@ class Scene2 extends Phaser.Scene {
     ship.x = randomX;
   }
 
-  update() {
-    this.moveShip(this.ship1, 1);
-    this.moveShip(this.ship2, 2);
-    this.moveShip(this.ship3, 3);
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode"); 
   }
 }
